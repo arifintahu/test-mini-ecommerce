77968fffe4bb65c3dce792efc366d6194c0642a9
Miftahul Arifin <miftahulfin@gmail.com>
2019-05-17 14:12:38 +0700

    Updates

---
+++ precache-manifest.e0d12da14a2a53ff3cfa4c5334bf1154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bc8afce18f07b493babb61ba78b52c",
+    "url": "/test-mini-ecommerce/index.html"
+  },
+  {
+    "revision": "7eba1701b0d64e48f3ee",
+    "url": "/test-mini-ecommerce/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "28e5f02bd846889732c4",
+    "url": "/test-mini-ecommerce/static/css/main.d5cc20b0.chunk.css"
+  },
+  {
+    "revision": "7eba1701b0d64e48f3ee",
+    "url": "/test-mini-ecommerce/static/js/2.b3c170be.chunk.js"
+  },
+  {
+    "revision": "28e5f02bd846889732c4",
+    "url": "/test-mini-ecommerce/static/js/main.1ce1f860.chunk.js"
+  },
+  {
+    "revision": "148ffbb8f9a0387ed3ff",
+    "url": "/test-mini-ecommerce/static/js/runtime~main.bcffbe41.js"
+  }
+]);
